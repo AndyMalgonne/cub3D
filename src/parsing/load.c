578bb4b144bf95ad35235c33f6d0dd9240c2612b
andymalgonne <andymalgonne@gmail.com>
2025-04-21 16:29:36 +0200

    load textures working but not normed ( wip )

--- src/parsing/load.c
+++ src/parsing/load.c
@@ -6,7 +6,7 @@
 /*   By: amalgonn <amalgonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:59:59 by amalgonn          #+#    #+#             */
-/*   Updated: 2025/04/14 14:45:52 by amalgonn         ###   ########.fr       */
+/*   Updated: 2025/04/21 16:27:47 by amalgonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,29 +71,49 @@ int	parse_color(char *line)
 	return (ft_fsplit(rgb), color);
 }
 
-int	load_textures(t_data *data)
+int load_textures(t_data *data)
 {
-	data->no = mlx_xpm_file_to_image(data->mlx, data->no,
-			&data->img_width, &data->img_height);
-	if (!data->no)
-		return (printf("Error\nFailed to load texture: NO (%s)\n",
-				data->no), 0);
-	data->so = mlx_xpm_file_to_image(data->mlx, data->so,
-			&data->img_width, &data->img_height);
-	if (!data->so)
-		return (printf("Error\nFailed to load texture: SO (%s)\n",
-				data->so), 0);
-	data->we = mlx_xpm_file_to_image(data->mlx, data->we,
-			&data->img_width, &data->img_height);
-	if (!data->we)
-		return (printf("Error\nFailed to load texture: WE (%s)\n",
-				data->we), 0);
-	data->ea = mlx_xpm_file_to_image(data->mlx, data->ea,
-			&data->img_width, &data->img_height);
-	if (!data->ea)
-		return (printf("Error\nFailed to load texture: EA (%s)\n",
-				data->ea), 0);
-	if (!data->no || !data->so || !data->we || !data->ea)
-		return (printf("Error\nFailed to load one or more textures\n"), 0);
+	int temp_bpp, temp_line_len, temp_endian;
+	data->textures->no_img = mlx_xpm_file_to_image(data->mlx, data->textures->no_path,
+												&data->textures->no_width, &data->textures->no_height);
+	if (!data->textures->no_img)
+		return (printf("Error\nFailed to load texture: NO (%s)\n", data->textures->no_path), 0);
+	data->textures->no_addr = mlx_get_data_addr(data->textures->no_img, &data->textures->bpp,
+											&data->textures->line_len, &data->textures->endian);
+	if (!data->textures->no_addr)
+		return (printf("Error\nFailed to get data address for NO texture\n"), 0);
+	data->textures->so_img = mlx_xpm_file_to_image(data->mlx, data->textures->so_path,
+												&data->textures->so_width, &data->textures->so_height);
+	if (!data->textures->so_img)
+		return (printf("Error\nFailed to load texture: SO (%s)\n", data->textures->so_path), 0);
+	data->textures->so_addr = mlx_get_data_addr(data->textures->so_img, &temp_bpp,
+											&temp_line_len, &temp_endian);
+	if (!data->textures->so_addr)
+		return (printf("Error\nFailed to get data address for SO texture\n"), 0);
+	if (temp_bpp != data->textures->bpp || temp_line_len != data->textures->line_len ||
+		temp_endian != data->textures->endian)
+		return (printf("Error\nInconsistent texture parameters for SO\n"), 0);
+	data->textures->we_img = mlx_xpm_file_to_image(data->mlx, data->textures->we_path,
+												&data->textures->we_width, &data->textures->we_height);
+	if (!data->textures->we_img)
+		return (printf("Error\nFailed to load texture: WE (%s)\n", data->textures->we_path), 0);
+	data->textures->we_addr = mlx_get_data_addr(data->textures->we_img, &temp_bpp,
+											&temp_line_len, &temp_endian);
+	if (!data->textures->we_addr)
+		return (printf("Error\nFailed to get data address for WE texture\n"), 0);
+	if (temp_bpp != data->textures->bpp || temp_line_len != data->textures->line_len ||
+		temp_endian != data->textures->endian)
+		return (printf("Error\nInconsistent texture parameters for WE\n"), 0);
+	data->textures->ea_img = mlx_xpm_file_to_image(data->mlx, data->textures->ea_path,
+												&data->textures->ea_width, &data->textures->ea_height);
+	if (!data->textures->ea_img)
+		return (printf("Error\nFailed to load texture: EA (%s)\n", data->textures->ea_path), 0);
+	data->textures->ea_addr = mlx_get_data_addr(data->textures->ea_img, &temp_bpp,
+											&temp_line_len, &temp_endian);
+	if (!data->textures->ea_addr)
+		return (printf("Error\nFailed to get data address for EA texture\n"), 0);
+	if (temp_bpp != data->textures->bpp || temp_line_len != data->textures->line_len ||
+		temp_endian != data->textures->endian)
+		return (printf("Error\nInconsistent texture parameters for EA\n"), 0);
 	return (1);
 }
